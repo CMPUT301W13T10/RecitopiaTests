3d84976ceb9c6cb1e1e2d0f95118842cd6d5ae2a
Alex Wilson <aewilson@ualberta.ca>
2013-02-28 15:54:18 -0700

    Add basic Recipe jUni test.

--- src/ca/teamTen/recitopia/test/RecipeTests.java
+++ src/ca/teamTen/recitopia/test/RecipeTests.java
@@ -1,5 +1,6 @@
 package ca.teamTen.recitopia.test;
 
+
 import ca.teamTen.recitopia.Recipe;
 import junit.framework.TestCase;
 
@@ -8,16 +9,25 @@ public class RecipeTests extends TestCase
 {
 
 	private Recipe recipe;
+	private String name = "spiky melon salad";
+	private String ingredients[] = {"spiky melon", "spices", "salad dresing"};
+	private String instructions = "make a salad";
+	private String author = "Clark Kent";
 	
 	protected void setUp() throws Exception
 	{
 		super.setUp();
 
-		recipe = new Recipe();
+		recipe = new Recipe(name, ingredients, instructions, author);
 	}
 
 	protected void tearDown() throws Exception
 	{
 		super.tearDown();
+	}
+	
+	public void testConstructor() {
+		assertEquals(recipe.getName(), name);
+		assertEquals(recipe.getAuthor(), author);
 	}
 }
