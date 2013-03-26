34a8866d06ba6856eb15bc351db0a064309c425a
Alex Wilson <yourpalal2@gmail.com>
2013-03-26 12:29:32 -0600

    Add test for SimpleRecipeBook persistence.
    Uses in-memory storage to avoid disk i/o.

--- src/ca/teamTen/recitopia/test/SimpleRecipeBookTest.java
+++ src/ca/teamTen/recitopia/test/SimpleRecipeBookTest.java
@@ -1,7 +1,15 @@
 package ca.teamTen.recitopia.test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import ca.teamTen.recitopia.Recipe;
 import ca.teamTen.recitopia.RecipeBook;
 import ca.teamTen.recitopia.SimpleRecipeBook;
+import ca.teamTen.recitopia.SimpleRecipeBook.IOFactory;
 
 public class SimpleRecipeBookTest extends RecipeBookTest {
 
@@ -10,7 +18,37 @@ public class SimpleRecipeBookTest extends RecipeBookTest {
 		return new SimpleRecipeBook();
 	}
 	
+	// create recipe book with memory-based IOFactory to test save/load.
 	public void testPersistence() {
-		// TODO: create recipe book here with memory-based IOFactory, test save/load.
+		recipeBook = new SimpleRecipeBook(new BufferIOFactory());
+		addTestData();
+		
+		// save and load data
+		recipeBook.save();
+		((SimpleRecipeBook)recipeBook).load();
+		
+		// now make sure all of the test data recipes are present
+		Recipe[] recipes = recipeBook.query("");
+		for (Recipe recipe: defaultRecipes) {
+			assertTrue(queryResultContains(recipes, recipe));
+		}
+	}
+	
+	/*
+	 * SimpleRecipeBook.IOFactory that reads/writes to memory.
+	 */
+	class BufferIOFactory implements IOFactory {
+		private ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		
+		@Override
+		public InputStream getInputStream() throws IOException {
+			return new ByteArrayInputStream(outStream.toByteArray());
+		}
+
+		@Override
+		public OutputStream getOutputStream() throws IOException {
+			return outStream;
+		}
+		
 	}
 }
